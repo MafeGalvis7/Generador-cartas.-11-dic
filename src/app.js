345541c31160d32b771a7b0bac5559801eeb2702
MafeGalvis7 <mariafernandagalvis33@hotmail.com>
2024-12-27 14:49:55 +0000

    generador cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { read } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomCard() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let rdmSuits = suits[Math.floor(Math.random() * suits.length)];
+    let rdmNumber = number[Math.floor(Math.random() * number.length)];
+
+    let color = rdmSuits === "♦" || rdmSuits == "♥" ? "red" : "black";
+
+    document.querySelector(".suits-top").innerHTML = rdmSuits;
+    document.querySelector(".number").innerHTML = rdmNumber;
+    document.querySelector(".suits-bottom").innerHTML = rdmSuits;
+
+    document.querySelector(".suits-top").style.color = color;
+    document.querySelector(".suits-bottom").style.color = color;
+  }
+  randomCard();
 };
